5a70bc3d774285fcfd8cea7ce962900f8c00be63
Gastrash <gastrashxd501@gmail.com>
2026-04-01 20:41:04 -0300

    see

---
+++ JS/galeria.js
@@ -0,0 +1,23 @@
+//galeria de cortes
+
+const tooltip = document.querySelector(".tooltip");
+
+const imgs = document.querySelectorAll(".grid-container img");
+
+imgs.forEach(img => {
+    img.addEventListener("mouseenter", (e) => {
+        img.classList.add("hovered");
+        tooltip.classList.add("active");
+        tooltip.textContent = img.dataset.info;
+    });
+
+    img.addEventListener("mousemove", (e) => {
+        tooltip.style.left = e.pageX + 10 + "px";
+        tooltip.style.top = e.pageY + 10 + "px";
+    });
+
+    img.addEventListener("mouseleave", () => {
+        img.classList.remove("hovered");
+        tooltip.classList.remove("active");
+    });
+});
